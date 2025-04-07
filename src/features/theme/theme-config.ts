ea3b2b4d8739ef10e0e631291289fc14839cfdfb
michael.jefferies <michael.jefferies@dairylandpower.com>
2025-04-07 13:26:04 -0500

    Parameterize theme.

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,12 @@
 export const AI_NAME = "VoltWrite";
-export const AI_DESCRIPTION = "VoltWrite is a friendly AI assistant.";
+export const AI_DESCRIPTION = `${AI_NAME} is a friendly AI assistant.`;
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant for Freeborn Mower Electric Cooperative. 
+//Example: " for Freeborn Mower Electric Cooperative"
+export const FOR_CLIENT_CLAUSE = ""
+
+
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant${FOR_CLIENT_CLAUSE}. 
 You're expected to communicate in a professional, yet friendly tone and have a solid understanding of the electric cooperative industry. 
 You should be prepared to handle a diverse range of inquiries and tasks across all levels of the organization. 
 When you need more information to perform a task effectively, don't hesitate to ask the user for additional context. You must always return in markdown format.
